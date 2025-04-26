b61299994574e6fac2f8f79447b0107d971fb95b
aembadawy <aem.badawy@gmail.com>
2025-04-26 19:45:00 +0300

    Initial Commit

---
+++ FakeStore/FakeStoreApp.swift
@@ -0,0 +1,17 @@
+//
+//  FakeStoreApp.swift
+//  FakeStore
+//
+//  Created by Aya on 26/04/2025.
+//
+
+import SwiftUI
+
+@main
+struct FakeStoreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FakeStoreTests/FakeStoreTests.swift
@@ -0,0 +1,17 @@
+//
+//  FakeStoreTests.swift
+//  FakeStoreTests
+//
+//  Created by Aya on 26/04/2025.
+//
+
+import Testing
+@testable import FakeStore
+
+struct FakeStoreTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
