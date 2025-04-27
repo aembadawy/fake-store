d4cfc4e174d61fc6d8aa14ce82487e80476ea5b5
aembadawy <aem.badawy@gmail.com>
2025-04-27 11:51:07 +0300

    Add data models

---
+++ FakeStore/Model/Product.swift
@@ -0,0 +1,8 @@
+//
+//  Product.swift
+//  FakeStore
+//
+//  Created by Aya on 27/04/2025.
+//
+
+import Foundation
---
+++ FakeStore/Model/User.swift
@@ -0,0 +1,8 @@
+//
+//  User.swift
+//  FakeStore
+//
+//  Created by Aya on 27/04/2025.
+//
+
+import Foundation
