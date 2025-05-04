dc52497cd0e1be34eae45f3e4b7f5c99a62ab180
aembadawy <aem.badawy@gmail.com>
2025-05-04 18:35:31 +0300

    Fetch real users from api

--- FakeStore/Service/UsersService.swift
+++ FakeStore/Service/UsersService.swift
@@ -9,8 +9,23 @@ import Foundation
 
 class UsersService {
     
+    let urlString = "https://fakestoreapi.com/users"
+    
     func fetchUsers() async throws -> [User] {
-        try await Task.sleep(for: .seconds(2))
-        return User.mockUsers
+        guard let url = URL(string: urlString) else { throw URLError(.badURL) }
+        
+        let (data, response) = try await URLSession.shared.data(from: url)
+        try validateResponse(response)
+        return try JSONDecoder().decode([User].self, from: data)
+    }
+    
+    func validateResponse(_ response: URLResponse) throws {
+        guard let httpResponse = response as? HTTPURLResponse else {
+            throw URLError(.badServerResponse)
+        }
+        
+        guard httpResponse.statusCode == 200 else {
+            throw URLError(.badServerResponse)
+        }
     }
 }
