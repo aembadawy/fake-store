00afaaed8d43d5837cac16fb45f30785e61393e4
aembadawy <aem.badawy@gmail.com>
2025-05-06 09:05:28 +0300

    Add cache for products

---
+++ FakeStore/Cache/ProductCache.swift
@@ -0,0 +1,47 @@
+//
+//  ProductCache.swift
+//  FakeStore
+//
+//  Created by Aya on 06/05/2025.
+//
+
+import Foundation
+
+struct ProductCache {
+    let documentDirURL = FileManager.default.urls(
+        for: .documentDirectory,
+        in: .userDomainMask
+    ).first
+    
+    let fileName = "products.json"
+    
+    // can also be written with the do catch approche
+    //which is better since we don't have to proceed this func call with a try
+    
+    func getProducts() throws -> [Product] {
+        guard let fileUrl = documentDirURL?.appending(path: fileName) else { return [] }
+        let data = try Data(contentsOf: fileUrl)
+        return try JSONDecoder().decode([Product].self, from: data)
+    }
+    
+    func saveProducts(_ products: [Product]) {
+        guard let fileUrl = documentDirURL?.appending(path: fileName) else { return }
+        
+        do {
+            let data = try JSONEncoder().encode(products)
+            try data.write(to: fileUrl)
+        } catch {
+            print("Failed to save products") // or log
+        }
+    }
+    
+    func invalidateCache() {
+        guard let fileURL = documentDirURL?.appending(path: fileName) else { return }
+        
+        do {
+            try FileManager.default.removeItem(at: fileURL)
+        } catch {
+            print("Failed to save products") // or log
+        }
+    }
+}
